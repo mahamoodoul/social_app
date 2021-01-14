bdeff6278581e896ff0a4f8d6ec889be432d3db1
shakil <mahamodul.shakil11@gmail.com>
2021-01-15 00:16:25 +0600

    added authentication system and maint user profile

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import HttpResponse, render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
+
+@login_required
+def home(request):
+    if request.method == 'GET':
+        search = request.GET.get('search', '')
+        result = User.objects.filter(username__icontains=search)
+    return render(request, 'App_Posts/home.html', context={'title':'Home','search':search, 'result':result})
